4c5f57ec15f1085cde17a70c8d361864e4958f67
Christopher Messner <christopher.messner@smail.fh-koeln.de>
2017-10-01 00:05:53 +0200

    Refactoring neuerCookie + Designanpassungen

--- glueckskeks.js
+++ glueckskeks.js
@@ -15,64 +15,63 @@ function unfade(element) {
     }, 10);
 }
 
-function getTerm() {
+var terms, section;
 
-	var section = document.querySelector("#buzzword");
-	// section.classList.remove('show');
-
-	var terms, type, category, buzzwordDE, buzzwordEN, abbreviation, relatesTo, description, example, number;
-
-	number = parseInt(getRandomInt(0,3));
-	console.log(number);
-	// number = 0;
-
+function init() {
+	section = document.querySelector("#buzzword");
 	var oReq = new XMLHttpRequest();
 	oReq.open("get", "https://raw.githubusercontent.com/wordbearer/uxbuzz/master/terms.json", true); 
 	oReq.responseType = 'json';
 	oReq.send();
 
 	oReq.onload = function() {
-
   		terms=oReq.response;
+  		getTerm();
+  	}
+}
 
-  		buzzwordEN = document.getElementById("term_buzzwordEN");
-		buzzwordEN.innerHTML = terms.buzzwords[number].buzzwordEN;
+function getTerm() {
 
-		if (terms.buzzwords[number].buzzwordDE){
-			buzzwordDE = document.getElementById("term_buzzwordDE");
-			buzzwordDE.innerHTML = "/ "+terms.buzzwords[number].buzzwordDE;
-		}
-		else{
-			buzzwordDE = document.getElementById("term_buzzwordDE");
-			buzzwordDE.innerHTML = "";
-		}
+	var type, category, buzzwordDE, buzzwordEN, abbreviation, relatesTo, description, example, number;
 
-		if (terms.buzzwords[number].abbreviation){
-			abbreviation = document.getElementById("term_abbreviation");
-			abbreviation.innerHTML = "("+terms.buzzwords[number].abbreviation+")";
-		}
-		else{
-			abbreviation = document.getElementById("term_abbreviation");
-			abbreviation.innerHTML = "";
-		}
+	number = parseInt(getRandomInt(0,3));
+	console.log(number);
+	// number = 0;
 
-  		description = document.getElementById("term_description");
-		description.innerHTML = terms.buzzwords[number].description;
+	buzzwordEN = document.getElementById("term_buzzwordEN");
+	buzzwordEN.innerHTML = terms.buzzwords[number].buzzwordEN;
 
-  		example = document.getElementById("term_example");
-		example.innerHTML = terms.buzzwords[number].example;
-
-  		type = document.getElementById("term_type");
-		type.innerHTML = terms.buzzwords[number].type;
-
-		category = document.getElementById("term_category");
-		category.innerHTML = terms.buzzwords[number].category;
-
-		unfade(section);
-
-		// section.classList.add('show');
-
+	if (terms.buzzwords[number].buzzwordDE){
+		buzzwordDE = document.getElementById("term_buzzwordDE");
+		buzzwordDE.innerHTML = "/ "+terms.buzzwords[number].buzzwordDE;
 	}
+	else{
+		buzzwordDE = document.getElementById("term_buzzwordDE");
+		buzzwordDE.innerHTML = "";
+	}
+
+	if (terms.buzzwords[number].abbreviation){
+		abbreviation = document.getElementById("term_abbreviation");
+		abbreviation.innerHTML = "("+terms.buzzwords[number].abbreviation+")";
+	}
+	else{
+		abbreviation = document.getElementById("term_abbreviation");
+		abbreviation.innerHTML = "";
+	}
+
+		description = document.getElementById("term_description");
+	description.innerHTML = terms.buzzwords[number].description;
+
+		example = document.getElementById("term_example");
+	example.innerHTML = terms.buzzwords[number].example;
+
+		type = document.getElementById("term_type");
+	type.innerHTML = terms.buzzwords[number].type;
+
+	category = document.getElementById("term_category");
+	category.innerHTML = terms.buzzwords[number].category;
+
+	unfade(section);
 
 }
 
@@ -80,7 +79,8 @@ var newCookie  = document.getElementsByClassName('newcookie');
 
 var i;
 for (i = 0; i < newCookie.length; i++) {
-    newCookie[i].addEventListener ('click', getTerm, true);	
+    newCookie[i].addEventListener ('click', getTerm, true);
+    newCookie[i].style.cursor="pointer";	
 }
 
-window.addEventListener('load', getTerm);
+window.addEventListener('load', init);
---
+++ sub.js
@@ -0,0 +1,7 @@
+var backhome  = document.getElementById('backhome');
+backhome.addEventListener ('click', abnachhause, true);
+backhome.style.cursor="pointer";		
+
+function abnachhause(){
+	window.open("index.html","_self")
+}
