f212dea962cbec2c363e0e0940989f3ff75e39c2
Christopher Messner <christopher.messner@smail.fh-koeln.de>
2017-09-30 21:16:01 +0200

    small adjustments json + cleanup js

--- glueckskeks.js
+++ glueckskeks.js
@@ -4,40 +4,23 @@ function getRandomInt(min, max) {
 
 function getTerm() {
 
-	// var fr;
-	// fr = new FileReader();
- //    //fr.onload = receivedText;
- //    fr.readAsText("terms.json");
-
-	// var terms = JSON.parse(fr);
-
-	var treeData;
+	var terms;
 
 	var oReq = new XMLHttpRequest();
-	//oReq.onload = reqListener;
-	oReq.open("get", "http://from-my-experience.com/blogs/media/terms.json", true); 
-	//oReq.open("get", "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json", true);
+	oReq.open("get", "https://raw.githubusercontent.com/wordbearer/uxbuzz/master/terms.json", true); 
 	oReq.responseType = 'json';
 	oReq.send();
 
 	oReq.onload = function() {
-  		treeData=oReq.response;
-  		console.log(treeData);
-  		console.log(treeData['squadName']);
-  		//treeData = JSON.parse(oReq.response);
-
+  		terms=oReq.response;
+  		console.log(terms);
+  		console.log(terms['buzzwords']);
 	}
-
-	// function reqListener(e) {
-	//     treeData = JSON.parse(this.responseText);
-	// }
-
 
 
 	var number;
 	number = document.getElementById("term_type");
 	number.innerHTML = getRandomInt(1,3);
-	//number.innerHTML = terms;
 }
 
 var newCookie  = document.getElementById('newcookie');
