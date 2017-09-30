76882d435382a58342c0f9232664cc0ad6cba992
Christopher Messner <christopher.messner@smail.fh-koeln.de>
2017-09-30 21:31:57 +0200

    json adaptions + js refinement

--- glueckskeks.js
+++ glueckskeks.js
@@ -4,7 +4,11 @@ function getRandomInt(min, max) {
 
 function getTerm() {
 
-	var terms;
+	var terms, type, category, buzzwordDE, buzzwordEN, abbreviation, relatesTo, description, example, number;
+
+	// number = parseInt(getRandomInt(0,2));
+	// console.log(number);
+	number = 0;
 
 	var oReq = new XMLHttpRequest();
 	oReq.open("get", "https://raw.githubusercontent.com/wordbearer/uxbuzz/master/terms.json", true); 
@@ -12,15 +16,26 @@ function getTerm() {
 	oReq.send();
 
 	oReq.onload = function() {
+
   		terms=oReq.response;
-  		console.log(terms);
-  		console.log(terms['buzzwords']);
+  		// console.log(terms);
+  		// console.log(terms.buzzwords[0].type);
+
+  		example = document.getElementById("term_example");
+		example.innerHTML = terms.buzzwords[number].example;
+
+  		type = document.getElementById("term_type");
+		type.innerHTML = terms.buzzwords[number].type;
+
+		category = document.getElementById("term_category");
+		category.innerHTML = terms.buzzwords[number].category;
+
 	}
 
 
-	var number;
-	number = document.getElementById("term_type");
-	number.innerHTML = getRandomInt(1,3);
+	// var number;
+	// number = document.getElementById("term_type");
+	// number.innerHTML = getRandomInt(1,3);
 }
 
 var newCookie  = document.getElementById('newcookie');
